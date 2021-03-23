d503932de0c66af8d2c37137aec87ff0891d96be
galaxyme-github <littleyoungman2019@outlook.com>
2021-03-22 23:43:51 -0700

    db_config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'ngproc_wp450' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Ja6hEcQWhh%mG@$2$EITBBr3' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
